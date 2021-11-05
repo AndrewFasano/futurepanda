a0ef1c491626cafcc76aa61534e401f935ef9767
Andrew Fasano <fasano@mit.edu>
2021-11-05 11:42:06 -0400

    Start of callstack_instr port

--- panda/callstack_instr/callstack_instr.h
+++ panda/callstack_instr/callstack_instr.h
@@ -3,14 +3,42 @@
 
 #include "prog_point.h"
 
-// BEGIN_PYPANDA_NEEDS_THIS -- do not delete this comment bc pypanda
-// api autogen needs it.  And don't put any compiler directives
-// between this and END_PYPANDA_NEEDS_THIS except includes of other
-// files in this directory that contain subsections like this one.
+// TODO: add the typedef macro so it generates the function + typedef
 
-PPP_CB_TYPEDEF(void, on_call, CPUState *env, target_ulong func);
-PPP_CB_TYPEDEF(void, on_ret, CPUState *env, target_ulong func);
+//PPP_CB_TYPEDEF(void, on_call, CPUState *env, target_ulong func);
+//PPP_CB_TYPEDEF(void, on_ret, CPUState *env, target_ulong func);
+void on_call(CPUState *env, target_ulong func);
+typedef void (*on_call_t)(CPUState *env, target_ulong func);
+void on_ret(CPUState *env, target_ulong func);
+typedef void (*on_ret_t)(CPUState *env, target_ulong func);
 
-// END_PYPANDA_NEEDS_THIS -- do not delete this comment!
+// Public interface
 
+// Get up to n callers from the given stack in use at this moment
+// Callers are returned in callers[], most recent first
+//uint32_t get_callers(target_ulong *callers, uint32_t n, CPUState *cpu);
+
+// Get up to n functions from the given stack in use at this moment
+// Functions are returned in functions[], most recent first
+//uint32_t get_functions(target_ulong *functions, uint32_t n, CPUState *cpu);
+
+// NB: prog_point is c++, so beware
+
+// Get the current program point: (Caller, PC, stack type, stack ID components)
+// This isn't quite the right place for it, but since it's awkward
+// right now to have a "utilities" library, this will have to do
+//void get_prog_point(CPUState *cpu, prog_point *p);
+
+// create pandalog message for callstack info
+//Panda__CallStack *pandalog_callstack_create(void);
+
+// free that data structure
+//void pandalog_callstack_free(Panda__CallStack *cs);
 #endif
+
+#ifndef PLUGIN_MAIN
+// TODO: unify this macro to support a list of fn names and generate a single constructor
+PPP_IMPORT(callstack_instr, on_call);
+PPP_IMPORT(callstack_instr, on_ret);
+#endif
+
