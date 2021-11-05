97b587bef1a0661ab6bad1f13016ea573566a1ab
Andrew Fasano <fasano@mit.edu>
2021-11-05 11:47:30 -0400

    Revert "plugins: Drop tcg_flags from struct qemu_plugin_dyn_cb"
    This reverts commit c7bb41b44ad63515adccffa8a6486da46b0c2e26.

--- include/qemu/plugin.h
+++ include/qemu/plugin.h
@@ -79,6 +79,7 @@ enum plugin_dyn_cb_subtype {
 struct qemu_plugin_dyn_cb {
     union qemu_plugin_cb_sig f;
     void *userp;
+    unsigned tcg_flags;
     enum plugin_dyn_cb_subtype type;
     /* @rw applies to mem callbacks only (both regular and inline) */
     enum qemu_plugin_mem_rw rw;
