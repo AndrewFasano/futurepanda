db4fcda502a0f967f290e1b5802ff8e9a9efec13
Andrew Fasano <fasano@mit.edu>
2023-01-04 14:33:45 -0500

    Add hw_proc_id to handle mips r28 kernel stack

---
+++ contrib/plugins/hw_proc_id.h
@@ -0,0 +1,10 @@
+#ifndef HW_PROC_ID_H
+#define HW_PROC_ID_H
+
+/*
+ * Prototypes for the do_add and do_sub functions. Both return an int and
+ * take an int as an argument.
+ */
+QPP_FUN_PROTOTYPE(hw_proc_id, uint64_t, hw_proc_id_get, void);
+
+#endif /* HW_PROC_ID_H */
